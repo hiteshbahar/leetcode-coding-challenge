7df34f7a4ba838ddcd4eb54fb71235fba507ed91
Xin Wang <xinwang@Xins-MBP.wv.cc.cmu.edu>
2017-03-26 12:26:47 -0400

    quicksort+mergesort

--- Sort.java
+++ Sort.java
@@ -1,5 +1,41 @@
 
 public class Sort {
+    public int[] mergeSort(int[] unsorted) {
+        if (unsorted.length <= 1) {
+            return unsorted;
+        }
+        int mid = unsorted.length / 2;
+        int[] left = new int[mid];
+        System.arraycopy(unsorted, 0, left, 0, mid);
+        int[] right = new int[unsorted.length - mid];
+        System.arraycopy(unsorted, mid, right, 0, unsorted.length - mid);
+        left = mergeSort(left);
+        right = mergeSort(right);
+        return merge(left, right);
+    }
+    public int[] merge(int[] left, int[] right) {
+        int[] merged = new int[left.length + right.length];
+        int indexLeft = 0;
+        int indexRight = 0;
+        int indexMerged = 0;
+        while (indexLeft < left.length && indexRight < right.length) {
+            if (left[indexLeft] < right[indexRight]) {
+                merged[indexMerged] = left[indexLeft];
+                indexLeft++;
+            } else {
+                merged[indexMerged] = right[indexRight];
+                indexRight++;
+            }
+            indexMerged++;
+        }
+        while (indexLeft < left.length) {
+            merged[indexMerged++] = left[indexLeft++];
+        }
+        while (indexRight < right.length) {
+            merged[indexMerged++] = right[indexRight++];
+        }
+        return merged;
+    }
     public void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pivotIndex = partition(arr, low, high);
@@ -31,8 +67,9 @@ public class Sort {
     public static void main(String[] args) {
         // TODO Auto-generated method stub
         Sort s = new Sort();
-        int[] arr = new int[]{10, 80, 30, 40, 60};
-        s.quickSort(arr, 0, arr.length - 1);
+        int[] arr = new int[]{4, 9, 6, 8, 10};
+        //s.quickSort(arr, 0, arr.length - 1);
+        arr = s.mergeSort(arr);
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
