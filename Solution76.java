beceba5fbac83bc11ea953495da9f5a4b9569051
Xin Wang <wangxin94@163.com>
2017-05-17 23:37:17 -0400

    minimum window substring two pointers

--- Solution76.java
+++ Solution76.java
@@ -2,6 +2,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Solution76 {
+    public String minWindow2(String s, String t) {
+        int[] tMap = new int[256];
+        for (int i = 0; i < t.length(); i++) {
+            tMap[t.charAt(i)]++;
+        }
+        int count = t.length();
+        int start = 0, end = 0;
+        int min = Integer.MAX_VALUE;
+        int head = 0;
+        
+        while (end < s.length()) {
+            char c = s.charAt(end);
+            if (tMap[c] > 0) {      // char in t
+                count--;               
+            }
+            tMap[c]--; 
+            end++;
+            while (count == 0) {      // update start pointer
+                if (end - start < min) {
+                    min = end - start;
+                    head = start;
+                }
+                char ch = s.charAt(start);
+                tMap[ch]++;
+                if (tMap[ch] > 0) {
+                    count++;              
+                }
+                start++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? "":s.substring(head, head + min);
+    }
+    
     /*Version 1: O(N^2) TLE too slow*/
     public String minWindow(String s, String t) {
         if (s.length() < t.length()) {
@@ -44,6 +77,8 @@ public class Solution76 {
         Solution76 slu = new Solution76();
         String s = "ADOBECODEBANC";
         String t = "ABC";
-        System.out.println(slu.minWindow(s, t));
+        System.out.print(slu.minWindow(s, t));
+        System.out.println();
+        System.out.println(slu.minWindow2(s, t));
     }
 }
