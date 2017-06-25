2bd70f8168c16a209737b914430dcf1005aabfdd
Xin Wang <xwang@purestorage.com>
2017-06-24 18:34:33 -0700

    solve conflict

--- PocketGems.java
+++ PocketGems.java
@@ -19,11 +19,11 @@ public class PocketGems {
                 return p1.red - p2.red;
             }
         });
-        
+
         for (int i = 0; i < pixels.length; i++) {
             queue.offer(pixels[i]);
         }
-        
+
         for (int i = 0; i < pixels.length; i++) {
             pixels[i] = queue.poll();
         }
@@ -49,7 +49,7 @@ public class PocketGems {
             }
         }
     }
-    
+
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         PocketGems p = new PocketGems();
@@ -67,5 +67,47 @@ public class PocketGems {
         }
         long end = System.currentTimeMillis();
         System.out.println(end - start);
+    }
+}
+/**
+ * Created by xwang on 6/18/17.
+ */
+public class PocketGems {
+    class Node {
+        int val;
+        Node left, right;
+        Node(int v) {
+            val = v;
+            left = null;
+            right = null;
+        }
+    }
+
+    public int findMax(Node root) {
+        int[] res = new int[1];
+        findMaxRootToLeaf(root, res, 0);
+        return res[0];
+    }
+
+    public void findMaxRootToLeaf(Node root, int[] res, int currSum) {
+        if (root == null) {
+            return;
+        }
+        currSum = root.val + currSum;
+        // reach leaf
+        if (root.left == null && root.right == null) {
+            res[0] = Math.max(currSum, res[0]);
+        }
+        findMaxRootToLeaf(root.left, res, currSum);
+        findMaxRootToLeaf(root.right, res, currSum);
+    }
+    public static void main (String[] args) {
+        PocketGems p = new PocketGems();
+        Node root = p.new Node(10);
+        root.left = p.new Node(-2);
+        root.right = p.new Node(7);
+        root.left.left = p.new Node(8);
+        root.left.right = p.new Node(-4);
+        System.out.println("the maximum is " + p.findMax(root));
     }
 }
