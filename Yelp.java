edef50f817038ed1b77a5785af8debbaa9bd89bb
Xin Wang <wangxin94@163.com>
2017-08-26 16:02:50 -0400

    add v12 yelp

--- Yelp.java
+++ Yelp.java
@@ -7,11 +7,42 @@ import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class Yelp {
+    /* V12: */
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
 
-    // maintain a max value, every time when there is a number larger
-    // than max, update the result list. When there is a tie, add the
-    // word to the result. Declare a comparator in the PriorityQueue,
-    // so that we can sort the result alphabetically.
+        List<String> list = new ArrayList<String>();
+        int minSum = Integer.MAX_VALUE;
+        for (int i = 0; i < list2.length; i++) {
+            Integer j = map.get(list2[i]);
+            if (j != null && i + j <= minSum) {
+                if (i + j < minSum) {
+                    list = new ArrayList<String>();
+                    minSum = i + j;
+                }
+                list.add(list2[i]);
+            }
+        }
+        // if cannot find the restaurant, return yelpwich
+//        if (list.size() == 0) {
+//            return "yelpwich";
+//        }
+        String[] result = new String[list.size()];
+        int i = 0;
+        for (String l : list) {
+            result[i++] = l;
+        }
+        return result;
+    }
+    
+    /* V11:
+       maintain a max value, every time when there is a number larger
+       than max, update the result list. When there is a tie, add the
+       word to the result. Declare a comparator in the PriorityQueue,
+       so that we can sort the result alphabetically.*/
     public List<String> getTopColors(List<List<String>> image) {
         Map<String, Integer> map = new HashMap<String, Integer>();
         // O(mn), the total number of words in the input image
