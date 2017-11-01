3f2e322a42645b900fad825cdf193e6eb85d53aa
Xin Wang <wangxin94@163.com>
2017-10-31 22:19:15 -0400

    rename the function

--- Petuum.java
+++ Petuum.java
@@ -1,7 +1,7 @@
 
 public class Petuum {
 
-    public static String removeDuplicateLetters(String str) {
+    public static String removeSameAdjacentLetters(String str) {
         if (str == null || str.length() <= 1) {
             return str;
         }
@@ -9,6 +9,7 @@ public class Petuum {
         int k = 0;
         char[] strArr = str.toCharArray();
         for (int i = 0; i < n; i++) {
+            // when length is equal to 0, it is always a new start
             if (k == 0 || strArr[i] != strArr[k - 1]) {
                 strArr[k++] = strArr[i];
             } else {
@@ -20,8 +21,8 @@ public class Petuum {
     public static void main(String[] args) {
         String str1 = "abbac";
         String str2 = "aaadcc";
-        System.out.println(removeDuplicateLetters(str1));
-        System.out.println(removeDuplicateLetters(str2));
+        System.out.println(removeSameAdjacentLetters(str1));
+        System.out.println(removeSameAdjacentLetters(str2));
 
     }
 
