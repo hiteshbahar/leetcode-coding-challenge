648406a7e99163cb269406caaffbfb63d8fa839b
Xin Wang <wangxin94@163.com>
2017-11-02 11:17:13 -0400

    test stones1 correct

--- Microsoft.java
+++ Microsoft.java
@@ -19,12 +19,43 @@ public class Microsoft {
         }
         return pos == stones.length - 1;
     }
+    public static boolean canCrossBinary(int[] stones, int pos, int k) {
+        for (int i = pos + 1; i < stones.length; i++) {
+            int gap = i - pos;
+            if (gap < k - 1) {
+                continue;
+            }
+            if (gap > k + 1) {
+                return false;
+            }
+            if (canCrossBinary(stones, i, gap)) {
+                return true;
+            }
+        }
+        return pos == stones.length - 1;
+    }
     public static void main(String[] args) {
         int[] stones1 = new int[]{0,1,3,5,6,8,12,17};
         int[] stones2 = new int[]{0,1,2,3,4,8,9,11};
+        int[] river = new int[stones1[stones1.length - 1] + 1];
+        
+        int stoneIndex = 0;
+        for (int i = 0; i < river.length; i++) {
+            if (i == stones1[stoneIndex]) {
+                river[i] = 1;
+                stoneIndex++;
+            }
+        }
 
         System.out.println(canCross(stones1));
         System.out.println(canCross(stones2));
+        
+        for (int i = 0; i < river.length; i++) {
+            System.out.print(river[i] + " ");
+        }
+        System.out.println();
+        System.out.println(canCrossBinary(river, 0, 0));
+        
 
     }
 
