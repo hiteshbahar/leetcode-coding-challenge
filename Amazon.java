2576532f96aa4bf89fbaa4bc2f3feafbc1f76a41
xinwang <xinw3@users.noreply.github.com>
2018-11-13 13:07:55 -0600

    add find substrings with k distinct characters from a string

--- Amazon.java
+++ Amazon.java
@@ -110,4 +110,36 @@ public class Amazon {
         }
         return result;
     }
+    
+    /**
+     * Given an inputStr and a number K, find all the substrings of size K with K distinct characters
+     * Substrings are not necessarily distinct
+     * @param inputStr  given string
+     * @param K the number of distinct characters in the string
+     * @return  a list of substrings
+     */
+    public static List<String> kDistinctCharacters(String inputStr, int K) {
+        List<String> result = new ArrayList<>();
+        if (inputStr == null || inputStr.length() < K) {
+            return result;
+        }
+        Set<Character> set = new HashSet<>();
+        int left = 0, right = 0;
+        while (right <= inputStr.length()) {
+            if (right - left == K) {
+                result.add(inputStr.substring(left, right));
+                if (right == inputStr.length()) break;
+                set.remove(inputStr.charAt(left));
+                left++;
+            }
+            char c = inputStr.charAt(right);
+            while (left < right && set.contains(c)) {
+                set.remove(inputStr.charAt(left));
+                left++;
+            }
+            set.add(c);
+
+            right++;
+        }
+        retur
 }
